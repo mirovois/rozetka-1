880a929f5efd18c3ccb6ef8f5b419bcb6d64828f
Andriy <andjey1990@gmail.com>
2014-08-03 10:39:44 +0300

    First commit

---
+++ src/main/java/com/atqc047/App.java
@@ -0,0 +1,13 @@
+package com.atqc047;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
